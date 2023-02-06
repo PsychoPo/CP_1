e4dfb412eb2f355125da1ad87a30f9940ec3a8ae
Roman Sukhachev <psychopo@icloud.com>
2023-02-07 00:19:44 +0700

    first commit

---
+++ lab1_sum.asm
@@ -0,0 +1,33 @@
+.model small
+.data
+a	dd ? ;
+b	dd ?
+msga	db 'Enter a = $'
+msgb	db 13,10,'Enter b = $'
+msgc	db 13,10,'Result a + b = $'
+.stack 256
+.code
+include Message.inc
+start:
+	mov ax,@data
+	mov ds,ax
+	mov dx,offset msga
+	call puts 
+	call inputfloat
+	fstp a
+
+	mov dx,offset msgb
+	call puts
+	call inputfloat
+	fstp b
+	
+	mov dx,offset msgc
+	call puts
+	fld a
+	fadd b
+	call outfloat
+	
+	call getch
+	mov ax,4c00h
+	int 21h
+end start
